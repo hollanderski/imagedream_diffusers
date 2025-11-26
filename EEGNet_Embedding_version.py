25f47f1be7d994a5a315be480f3e0b42dcb6cb93
unknown <lizeninon@yahoo.fr>
2025-11-26 08:21:46 -0800

    add base

--- EEGNet_Embedding_version.py
+++ EEGNet_Embedding_version.py
@@ -1,13 +1,56 @@
 import numpy as np
 import torch
-from torch import nn
+from torch import nn, optim
 from torch.nn.functional import elu
 import pytorch_lightning as pl
 
 import sys 
 sys.path.append('G:/ninon_workspace/imagery2024/2D_Reconstruction/Generation_2D/reconstruction/code/')
 
-from dc_ldm.models.base_model import base_model
+#from dc_ldm.models.base_model import base_model
+
+from torchmetrics.classification import Accuracy
+
+class base_model(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.CrossEntropyLoss()
+        self.acc = Accuracy(task="multiclass", num_classes=6)
+        self.config = None
+        self.save_hyperparameters()
+        self.one_cycle_lr = True
+        self.predictions = []
+        self.ground_truth = []
+
+    def training_step(self, batch, batch_idx):
+        x,y = batch
+        logit = self.forward(x.float()) 
+        train_loss = self.loss(logit, y)
+        _, y_pred = torch.max(logit, dim = 1)
+        self.log("train_loss", train_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_acc", self.acc(y_pred, y), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return train_loss
+        
+    def validation_step(self, batch, batch_idx):
+        x,y = batch
+        logit = self.forward(x.float()) 
+        val_loss = self.loss(logit, y)
+        _, y_pred = torch.max(logit, dim = 1)
+        self.log("val_loss", val_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_acc", self.acc(y_pred, y), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return val_loss
+    
+    def test_step(self, batch, batch_idx):
+        x,y = batch
+        print("TEST")
+        logit = self.forward(x.float())
+        test_loss = self.loss(logit, y)
+        _, y_pred = torch.max(logit, dim = 1)
+        self.predictions.append(y_pred)
+        self.ground_truth.append(y)
+        self.log("test_loss", test_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_acc", self.acc(y_pred, y), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return test_loss
 
 """
 Note: this code is adapted from https://github.com/braindecode/braindecode/tree/master/braindecode
